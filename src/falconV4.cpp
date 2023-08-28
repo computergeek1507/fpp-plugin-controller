5401df79559ae74c873e66b6d2804e856a1e6ce9
Scott Hanson <scooter_seh@yahoo.com>
2023-08-28 13:47:01 -0400

    working on falconv4

--- src/falconV4.cpp
+++ src/falconV4.cpp
@@ -24,15 +24,34 @@ FalconV4::~FalconV4() {
 }
 
 bool FalconV4::setTestModeOn() const {
-    //{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"transition_period":150,"mode":"normal","hue":120,"on_off":1,"saturation":65,"color_temp":0,"brightness":10}}}
+    //{"T":"S","M":"TS","B":0,"E":16,"I":0,"P":{"E":"Y","D":"Y","S":20,"Y":1,"A":[{"P":0,"R":0,"S":0},{"P":1,"R":0,"S":0},{"P":1,"R":0,"S":0},{"P":3,"R":0,"S":0},{"P":4,"R":0,"S":0}]}}
+    //{"T":"S","M":"TS","B":2,"E":16,"I":10,"P":{"E":"Y","D":"Y","S":20,"Y":1,"A":[{"P":10,"R":0,"S":0},{"P":11,"R":0,"S":0},{"P":12,"R":0,"S":0},{"P":13,"R":0,"S":0},{"P":14,"R":0,"S":0}]}}
+    static constexpr int PORT_PER = 5;
+    int numberOfCalls = std::ceil(m_outputs / PORT_PER);
+     bool test = true;
+
+    for (int j = 0; j < numberOfCalls; j++) {
+        int index2 = j * PORT_PER;
+        std::string cmd = "{\"T\":\"S\",\"M\":\"TS\",\"B\":" + std::to_string(j) + ",\"E\":16,\"I\":" + std::to_string(j) + ",\"P\":{\"E\":\"Y\",\"D\":\"Y\",\"S\":20,\"Y\":1,\"A\":[";
+        for (int i = 0; i < PORT_PER; i++) {
+            if(index2 + i >= m_outputs) {
+                break;
+            }
+            std::string cmd2 = "{\"P\":"+ std::to_string(index2 + i) +",\"R\":0,\"S\":0},";
+            cmd += cmd2;
+            
+        }
+        cmd.pop_back();//remove last comma
+        cmd += "]}}";
+        test &= postData("/api", cmd, "application/json");
+    }
     
-    //const std::string cmd = "{\"smartlife.iot.smartbulb.lightingservice\":{\"transition_light_state\":{\"ignore_default\":1,\"transition_period\":,\"mode\":\"normal\",\"hue\":,\"on_off\":1,\"saturation\":,\"color_temp\":,\"brightness\":}}}";
-    return true; //postData(cmd);
+    return test;
 }
 
 bool FalconV4::setTestModeOff() const {
 
-    //const std::string cmd = "{\"smartlife.iot.smartbulb.lightingservice\":{\"transition_light_state\":{\"ignore_default\":1,\"transition_period\":0,\"mode\":\"normal\",\"on_off\":0}}}";
-    return true; //postData(cmd);
+    const std::string cmd = R"({"T":"S","M":"TS","B":0,"E":16,"I":0,"P":{"E":"N","D":"Y","S":20,"Y":1,"A":[]}})";
+    return postData("/api", cmd, "application/json");
 }
 
--- src/genius.cpp
+++ src/genius.cpp
@@ -14,12 +14,10 @@ Genius::~Genius() {
 }
 
 bool Genius::setTestModeOn() const {
-    //{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"transition_period":150,"mode":"normal","hue":120,"on_off":1,"saturation":65,"color_temp":0,"brightness":10}}}
-    
-    const std::string url = "/api/test_mode_enable";
     std::string data;
+    const std::string url = "/api/test_mode_enable";
     bool test = getData(url, data, "application/json" );
-    const std::string url2 = "/api/set_test_elements??selected_elements={elements:[\"all\"]}";
+    const std::string url2 = "/api/set_test_elements?selected_elements={elements:[\"all\"]}";
     test &= getData(url2, data, "application/json" );
     return test;
 }
